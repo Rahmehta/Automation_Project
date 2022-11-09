69713af45a07bf63ece02c29c3e08541f757f701
Rahul Mehta <rahul1mehtaa@gmail.com>
2022-11-09 21:19:00 +0530

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -32,14 +32,15 @@ timeStamp=$(date '+%d%m%Y-%H%M%S')
 # making the tar file of log files and copying it on AWS S3 bucket
 tar -cvf /tmp/Rahul-httpd-$timeStamp.tar /var/log/apache2/ |aws s3 cp /tmp/Rahul-httpd-$timeStamp.tar  \s3://upgrad-rahul/Rahul-httpd-$timeStamp.tar
 
-#size = $(stat -c %s /tmp/Rahul-httpd-$timeStamp.tar)
+#get the size of tar file
+size = $(du -h /tmp/Rahul-httpd-$timeStamp.tar | awk '{ print $1 }')
 
 
 #check if inventory file exits and  appending with log data
 if [ -f /var/www/html/inventory.html ]
 then
-echo -e  "httpd-logs \t $timeStamp \t tar \t 10 KB  "  >> /var/www/html/inventory.html
+echo -e  "httpd-logs \t $timeStamp \t tar \t $size  "  >> /var/www/html/inventory.html
 else
 cat /var/www/html/inventory.html | echo -e 'Log Type \t Time Created \t \t Type \t Size' >> /var/www/html/inventory.html
-echo -e  "httpd-logs \t $timeStamp \t tar \t  10 KB  "  >> /var/www/html/inventory.html
+echo -e  "httpd-logs \t $timeStamp \t tar \t  $size  "  >> /var/www/html/inventory.html
 fi
